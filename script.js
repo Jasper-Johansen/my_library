27ac7527eeedeb02996c56482c716c37a41b8228
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-26 13:15:34 +0530

    create constructor for book

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const myLibrary = [];
 
 function Book(title,author,pages,read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+     this.pages = pages;
+     this.read = read;
 
-  this.readStatus = function(){
-    if (this.read === true) {
-        return "I read!";
-  }else{
-    return "I haven't read yet!";
-  }
+    this.readStatus = function(){
+        if (this.read === true) {
+            return "I read!";
+    }else{
+        return "I haven't read yet!";
+        }
+    }
 }
 
-const book1 = Book("Harry Potter I", "J K Rowling", 268, true);
 
-
-function addBookToLibrary() {
-  myLibrary.push(book1);
-  return myLibrary;
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+const book1 = new Book("Harry Potter I", "J K Rowling", 268, true);
+const book2 = new Book("Harry Potter II", "J K Rowling", 300, false);
 
-console.log(addBookToLibrary())
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+console.table(myLibrary);
