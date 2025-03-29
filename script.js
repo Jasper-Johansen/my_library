5bc3bf3e85d0c353f8d185ddac2208bcc0473ec9
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-29 12:46:43 +0530

    replace innerhtml of tableRow with cleaner code

--- script.js
+++ script.js
@@ -42,15 +42,36 @@ addButton.addEventListener("click", () => {
                 inputReadStatus.value.charAt(0).toUpperCase()+inputReadStatus.value.slice(1));
             myLibrary.push(newBook);
             const tableRow = document.createElement("tr");
-            tableRow.innerHTML = `
-            <td>${myLibrary.length+3}</td>
-            <td>${newBook.title}</td>
-            <td>${newBook.author}</td>
-            <td>${newBook.pages}</td>
-            <td>${newBook.readStatus}</td>
-            <td><button class="dlt">Delete</button></td>
-            `;
+
+            const idRow = document.createElement("td");
+            idRow.textContent = myLibrary.length;
+            tableRow.appendChild(idRow);
+
+            const titleRow = document.createElement("td");
+            titleRow.textContent = newBook.title;
+            tableRow.appendChild(titleRow);
+
+            const authorRow = document.createElement("td");
+            authorRow.textContent = newBook.author;
+            tableRow.appendChild(authorRow);
+
+            const pagesRow = document.createElement("td");
+            pagesRow.textContent = newBook.pages;
+            tableRow.appendChild(pagesRow);
+
+            const readStatusRow = document.createElement("td");
+            readStatusRow.textContent = newBook.readStatus;
+            tableRow.appendChild(readStatusRow);
+
+            const delRow = document.createElement("td");
+            const dltButton = document.createElement("button");
+            dltButton.classList.add("dlt");
+            dltButton.textContent = "Delete";
+            delRow.appendChild(dltButton);
+            tableRow.appendChild(delRow);
+
             tableBody.appendChild(tableRow);
+
             inputTitle.value = "";
             inputAuthor.value = "";
             inputPages.value= "";
