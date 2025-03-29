584e9b21a764d32caf1ded37b97aa8e02bcce47e
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-29 12:21:02 +0530

    add select option, user selection now appears in the table

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const toggleButton = document.querySelector(".toggle-button");
 const toggleForm = document.querySelector("#toggle-form");
 const delButtons = document.querySelectorAll(".dlt")
 const tableRow = document.querySelectorAll("tr");
-const addButton = document.querySelector(".add-btn")
+const addButton = document.querySelector(".add-btn");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputReadStatus = document.querySelector("#read");
+const tableBody = document.querySelector("tbody")
 
 toggleButton.addEventListener("click", () => {
     toggleForm.classList.toggle("hidden");
@@ -19,24 +24,38 @@ delButtons.forEach(button => {
     });
 });
 
-function Book(title,author,pages,read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    this.readStatus = function(){
-        if (this.read === true) {
-            return "I read!";
-    }else{
-        return "I haven't read yet!";
-        }
-    }
-}
-
 addButton.addEventListener("click", () => {
+
+    function Book(title,author,pages,readStatus) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
     
+    if(inputTitle!=="" || 
+        inputAuthor!=="" ||
+        inputPages!=="" ||
+        inputReadStatus.value!=="" ){
+            const newBook = new Book(inputTitle.value,inputAuthor.value,inputPages.value,
+                inputReadStatus.value.charAt(0).toUpperCase()+inputReadStatus.value.slice(1));
+            myLibrary.push(newBook);
+            const tableRow = document.createElement("tr");
+            tableRow.innerHTML = `
+            <td>${myLibrary.length+3}</td>
+            <td>${newBook.title}</td>
+            <td>${newBook.author}</td>
+            <td>${newBook.pages}</td>
+            <td>${newBook.readStatus}</td>
+            <td><button class="dlt">Delete</button></td>
+            `;
+            tableBody.appendChild(tableRow);
+            inputTitle.value = "";
+            inputAuthor.value = "";
+            inputPages.value= "";
+            inputReadStatus.checked = false;
+        }
 });
 
 
